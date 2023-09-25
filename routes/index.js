aeb704b9ff88a20bc0aae00e74eb3cc7b111d4a8
Duong Jonathan <djonathan91.77@gmail.com>
2023-09-25 12:12:05 +0200

    fix error: incorrect files name

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var express = require('express');
 var router = express.Router();
 
 // Routes
-const reservationsRouter = require('./reservations.js');
-const usersRouter = require('./users.js');
+const reservationsRouter = require('./reservations.routes.js');
+const usersRouter = require('./users.routes.js');
 
 router.use(reservationsRouter);
 router.use(usersRouter)
