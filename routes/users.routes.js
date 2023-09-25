984d759d19dd865aec5305b5eb2cccfdde442eab
Duong Jonathan <djonathan91.77@gmail.com>
2023-09-25 11:50:28 +0200

    implement routes: reservations & user + update index router

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,9 +1,17 @@
+const userController = require('../controllers/user.controller.js')
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// GET Users List
+router.get('/users', userController.findAll);
+
+// POST data
+router.post('/users', userController.create);
+
+// PUT data
+router.put('/users/:id', userController.update);
+
+// DELETE data
+router.delete('/users/:id', userController.delete);
 
 module.exports = router;
