eb25c8ee60416038c0fc1a13a4ce545b3b472b35
Duong Jonathan <djonathan91.77@gmail.com>
2023-09-26 09:30:35 +0200

    code style: add jwtMiddleware + export verifyJWT method

---
+++ middlewares/jwtMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const verifyJWT = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ auth: false, message: 'Token required' });
+
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (e) {
+        res.status(400).json({ auth: false, message: 'Incorrect token.' });
+    }
+};
+
+module.exports = verifyJWT;
--- routes/reservations.routes.js
+++ routes/reservations.routes.js
@@ -1,26 +1,8 @@
 require('dotenv').config();
 const reservationController = require('../controllers/reservation.controller.js');
-// Importation des modules nécessaires
+const verifyJWT = require('../middlewares/jwtMiddleware');
 const express = require('express');
-const jwt = require('jsonwebtoken');
-var router = express.Router();
-const SECRET_KEY = process.env.SECRET_KEY;
-
-
-
-const verifyJWT = (req, res, next) => {
-    const token = req.header('Authorization');
-
-    if (!token) return res.status(401).json({ auth: false, message: 'Token required' });
-
-    try {
-        const decoded = jwt.verify(token, SECRET_KEY);
-        req.user = decoded;
-        next();
-    } catch (e) {
-        res.status(400).json({ auth: false, message: 'Incorrect token.' });
-    }
-};
+const router = express.Router();
 
 // GET home page
 router.get('/reservations', reservationController.findAll);
