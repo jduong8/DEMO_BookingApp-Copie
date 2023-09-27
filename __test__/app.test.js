c059f270fb7c5851208181a9cbc034287031dae4
Duong Jonathan <djonathan91.77@gmail.com>
2023-09-27 16:28:24 +0200

    remove unnecessary code.

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,19 +1,11 @@
 const request = require('supertest');
 const app = require('../app.js');
 
-describe('GET /api/reservations', () => {
+describe('GET /api/reservations/all', () => {
     // Test le cas non authentifié
-    // it('should return a 401 error if user is not authenticated', async () => {
-    //     const res = await request(app)
-    //     .get('/api/reservations')
-    //     .expect('Content-Type', /json/)
-    //     .expect(401);
-
-     // Test la récupération des reservations
-    it('should return a 200', async () => {
+    it('should return a 401 error if user is not authenticated', async () => {
         const res = await request(app)
-        .get('/api/reservations')
+        .get('/api/reservations/all')
         .expect('Content-Type', /json/)
-        .expect(200);
-    }, 10000);
+        .expect(401)});
 });
