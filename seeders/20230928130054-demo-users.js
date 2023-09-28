030b648b9447e7e8c582ac8a0ee83f57d40c4d99
Duong Jonathan <djonathan91.77@gmail.com>
2023-09-28 15:53:38 +0200

    seed: add seeder for reservation & user - implement user mock data

---
+++ seeders/20230928130054-demo-users.js
@@ -0,0 +1,33 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    // Hashage du mot de passe
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash('securepassword', salt);
+    await queryInterface.bulkInsert('Users', [
+      {
+        user_role: 'Admin',
+        firstname: 'Clark',
+        lastname: 'Kent',
+        email: 'superman@example.com',
+        phone: '1234567890',
+        user_password: hashedPassword,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      // ... Autres utilisateurs
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
