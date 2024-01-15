1d7e1dcf6ee8250e219f69b2b95a7c24dde208e6
Duong Jonathan <djonathan91.77@gmail.com>
2024-01-15 11:31:43 +0100

    feat: add module cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -8,6 +9,7 @@ const errorHandler = require('./middlewares/errorHandler.middleware')
 
 var app = express();
 
+app.use(cors({ origin: 'http://localhost:8000' }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
