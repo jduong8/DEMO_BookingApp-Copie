58cd5b22685a6087ed6304fad2ae6362852be167
Duong Jonathan <djonathan91.77@gmail.com>
2024-03-11 16:43:22 +0100

    feat: add new status

--- models/reservationStatus.model.js
+++ models/reservationStatus.model.js
@@ -2,6 +2,7 @@ const RESERVATION_STATUS = {
   PENDING: "pending",
   CONFIRMED: "confirmed",
   CANCELLATION_REQUESTED: "cancellation_requested",
+  COMPLETED: "completed",
 };
 
 module.exports = RESERVATION_STATUS;
