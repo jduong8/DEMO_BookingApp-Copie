4c87f72de3aa0cf4f6ecc28bba1983bd5311750e
Duong Jonathan <djonathan91.77@gmail.com>
2024-03-12 14:14:39 +0100

    feat: implement delete service

--- controllers/place.controller.js
+++ controllers/place.controller.js
@@ -19,11 +19,9 @@ exports.create = async (req, res, next) => {
       req.user.user_role !== USER_ROLE.ADMIN &&
       req.user.user_role !== USER_ROLE.MASTER
     ) {
-      return res
-        .status(403)
-        .json({
-          message: "Access denied. Only admins and masters can create places.",
-        });
+      return res.status(403).json({
+        message: "Access denied. Only admins and masters can create places.",
+      });
     }
 
     // Vérifie que seats_count est un nombre et est positif
@@ -43,6 +41,40 @@ exports.create = async (req, res, next) => {
     res.send(placeResponse);
   } catch (error) {
     console.error("Place creation error:", error);
+    next(error);
+  }
+};
+
+exports.delete = async (req, res, next) => {
+  const { id } = req.params; // Récupération de l'identifiant de la place depuis les paramètres de la requête
+
+  try {
+    // Vérification si l'utilisateur a le rôle d'administrateur
+    if (req.user.user_role === USER_ROLE.CLIENT) {
+      return res.status(403).json({
+        message: "Access denied. Only admins can delete places.",
+      });
+    }
+
+    // Recherche du lieu à supprimer
+    const place = await Place.findByPk(id);
+
+    // Si la place n'existe pas, renvoyer une erreur 404
+    if (!place) {
+      return res.status(404).json({
+        message: "Place not found.",
+      });
+    }
+
+    // Suppression de la place
+    await place.destroy();
+
+    // Réponse indiquant que la suppression a été réalisée avec succès
+    res.status(200).json({
+      message: "Place deleted successfully.",
+    });
+  } catch (error) {
+    console.error("Place deletion error:", error);
     next(error);
   }
 };
