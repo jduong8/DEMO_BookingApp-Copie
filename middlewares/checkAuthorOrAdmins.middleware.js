ff0fab29aeb78de86558d160369fcae601943dfe
Jonathan Duong <djonathan91.77@gmail.com>
2024-03-18 09:38:04 +0100

    test matrix (WIP) delete user improvement (WIP)

--- middlewares/checkAuthorOrAdmins.middleware.js
+++ middlewares/checkAuthorOrAdmins.middleware.js
@@ -2,7 +2,7 @@ const USER_ROLE = require("../models/userRole.model.js");
 
 const checkAuthorOrAdminMiddleware =
   (model, modelName) => async (req, res, next) => {
-    const resourceId = req.params.id;
+    const resourceId = parseInt(req.params.id, 10);
     const currentUser = req.user;
 
     try {
@@ -11,18 +11,33 @@ const checkAuthorOrAdminMiddleware =
         return res.status(404).json({ message: `${modelName} not found.` });
       }
 
-      // Vérifie si l'utilisateur est l'auteur ou un administrateur
-      if (
-        currentUser.id !== resource.userId &&
-        currentUser.user_role !== USER_ROLE.ADMIN &&
-        currentUser.user_role !== USER_ROLE.MASTER
-      ) {
-        return res.status(403).json({ message: "Permission denied." });
+      // Autoriser le MASTER à effectuer n'importe quelle action.
+      if (currentUser.user_role === USER_ROLE.MASTER) {
+        return next();
       }
 
-      // On assigne la ressource à l'objet de requête
-      req.resource = resource;
-      next();
+      // Autoriser l'ADMIN à se gérer lui-même ou à gérer les CLIENTs uniquement.
+      if (currentUser.user_role === USER_ROLE.ADMIN) {
+        if (
+          currentUser.id === resourceId ||
+          resource.user_role === USER_ROLE.CLIENT
+        ) {
+          return next();
+        } else {
+          return res.status(403).json({ message: "Permission denied." });
+        }
+      }
+
+      // Autoriser le CLIENT à se gérer lui-même uniquement.
+      if (
+        currentUser.user_role === USER_ROLE.CLIENT &&
+        currentUser.id === resourceId
+      ) {
+        return next();
+      }
+
+      // Si aucune des conditions ci-dessus n'est remplie, l'accès est refusé.
+      return res.status(403).json({ message: "Permission denied." });
     } catch (error) {
       console.error("Middleware error:", error);
       res
